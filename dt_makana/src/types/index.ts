67f8ccdf034ab2efdb2604d81b83bc1b5274f324
Dxeg0o <dsoler.olguin@gmail.com>
2025-07-08 15:05:19 -0400

    tipos de eventos

---
+++ dt_makana/src/types/index.ts
@@ -0,0 +1,31 @@
+export interface BaseEvent {
+  department: string;
+  event_type: "license" | "accident" | "failure";
+  start_date: string;
+  uniqueness_flag: string;
+  turn_type: string;
+}
+export interface LicenseEvent extends BaseEvent {
+  license_type: string;
+  collaborator_age: number;
+  collaborator_gender: "male" | "female" | "other";
+  collaborator_seniority: string;
+}
+export interface AccidentEvent extends BaseEvent {
+  date: string;
+  hour: string;
+  accident_type: string;
+  activity: string;
+  motive: string;
+  severity: "No aplica" | "Baja" | "Media" | "Alta" | "Fatal";
+  potential: string;
+  collaborator_age: number;
+  collaborator_gender: "male" | "female" | "other";
+  collaborator_seniority: string;
+  description: string;
+}
+export interface FailureEvent extends BaseEvent {
+  date: string;
+  failure_type: string;
+}
+export type PersonnelEvent = LicenseEvent | AccidentEvent | FailureEvent;
